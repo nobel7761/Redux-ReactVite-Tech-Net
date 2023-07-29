912fbb7a2eaa11079f1bfaef069df58754c035c6
nobel7761 <habiburnobel@gmail.com>
2023-07-29 20:07:31 +0600

    react router dom configured

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h5 className="bg-red-500">Tech net</h5>;
+}
+
+export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import App from "../App";
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+export default routes;
