4745bd05b9b0b0db8ba969390909be0b9346843d
nobel7761 <habiburnobel@gmail.com>
2023-07-30 01:04:16 +0600

    404 page added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,25 @@
 import App from "../App";
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../page/Home";
+import Login from "../page/Login";
+import NotFound from "../page/NotFound";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/*",
+    element: <NotFound />,
   },
 ]);
 
